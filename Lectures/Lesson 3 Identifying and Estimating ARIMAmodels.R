d5a3f4699ffd267994b695f2a9e7419f428ea295
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-09-09 17:51:24 -0500

    add ARMAtoMA models, interesting !
    forecast, PI

--- Lectures/Lesson 3 Identifying and Estimating ARIMAmodels.R
+++ Lectures/Lesson 3 Identifying and Estimating ARIMAmodels.R
@@ -29,3 +29,13 @@ sarima.for(xerie, 4, 1, 0, 0)
 # 3.2 Diagnostics ---------------------------------------------------------
 
 
+# 3.3 Forecasting with ARIMA Models ----------------------------------------
+# Psi-weight representation of an ARIMA model
+# Example: Suppose that an AR(1) model is xt = 40 + 0.6xt-1 + wt
+ARMAtoMA(ar = 0.6, ma = 0, 12) # gives first 12 psi-weights, psi-0 = 1
+
+# Psi-Weights for the Estimated AR(2) for the Stride Length Data
+ARMAtoMA(ar = list(1.148, -0.3359), ma = 0, 5)
+
+# need to find treadmill data stride length
+
