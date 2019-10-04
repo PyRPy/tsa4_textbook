aab9a30bcbc2cc5e815c51d1cf0d7f39aa27c986
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-10-04 14:33:01 -0500

    add CO2 example

--- Lectures/Reading 4.3 Periodogram and Discrete Fourier Transform.R
+++ Lectures/Reading 4.3 Periodogram and Discrete Fourier Transform.R
@@ -16,3 +16,15 @@ mvspec(rec, log="no")
 abline(v=1/4, lty=2, col=8)
 mtext('1/4',side=1, line=0, at=.25, cex=.75)
 dev.off()
+
+# univariate example
+plot(co2)   # co2 is an R data set
+mvspec(co2, spans=c(5,5), taper=.5)
+
+# multivariate example
+ts.plot(mdeaths, fdeaths, col=1:2)   # an R data set, male/female monthly deaths ...
+dog = mvspec(cbind(mdeaths,fdeaths), spans=c(3,3), taper=.1)
+dog$fxx        # look a spectral matrix estimates
+dog$bandwidth  # bandwidth with time unit = year
+dog$bandwidth/frequency(mdeaths)  # ... with time unit = month
+plot(dog, plot.type="coherency")  # plot of squared coherency
