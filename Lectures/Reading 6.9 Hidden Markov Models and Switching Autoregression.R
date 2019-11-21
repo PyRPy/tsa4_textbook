dd3ed5483543b176d1fcd7d2bf5ef9badfc69a92
PyRPy <44626163+PyRPy@users.noreply.github.com>
2019-11-21 13:57:02 -0600

    add bootstrapped section for se - EQcount

--- Lectures/Reading 6.9 Hidden Markov Models and Switching Autoregression.R
+++ Lectures/Reading 6.9 Hidden Markov Models and Switching Autoregression.R
@@ -45,6 +45,44 @@ lines(xvals, u1, col=4)
 lines(xvals, u2, col=2)
 
 # boots trapping section is skipped for now
+# function to generate data
+pois.HHM.generate_sample <- function(n,m,lambda, Mtrans, StatDist=NULL){
+  # n = data length
+  # m = number of states
+  # Mtrans = transition matrix
+  # StatDist = stationary distn
+  if(is.null(StatDist)) StatDist=solve(t(diag(m)-Mtrans + 1),rep(1,m))
+  mvect = 1:m
+  state=numeric(n)
+  state[1]=sample(mvect, 1, prob = StatDist)
+  for (i in 2:n) state[i] = sample(mvect, 1, prob = Mtrans[state[i-1],])
+  y = rpois(n, lambda = lambda[state])
+  list(y=y, state=state)
+}
+
+# start it up
+set.seed(10101101)
+nboot <- 100
+nobs <- length(EQcount)
+para.star <- matrix(NA, nrow=nboot, ncol=6)
+for (j in 1:nboot){
+  x.star <- pois.HHM.generate_sample(n=nobs, m=2, lambda = lams, 
+                                     Mtrans = mtrans)$y
+  model <- depmix(x.star ~ 1, nstates = 2, data = data.frame(x.star),
+                  family = poisson())
+  
+  u <- as.vector(getpars(fit(model, verbose=0)))
+  
+  if (u[7] <= u[8]){para.star[j,] = c(u[3:6], exp(u[7]), exp(u[8]))} else
+  {para.star[j,] = c(u[6:3], exp(u[8]), exp(u[7]))}
+}
+
+# bootstrapped std errors
+SE <- sqrt(apply(para.star, 2, var) +
+             (apply(para.star, 2, mean) - para.mle)^2)[c(1,4:6)]
+
+names(SE) <- c('seM11/M12', 'seM21/M22', 'seLam1', 'seLam2')
+SE
 
 # Example 6.18 Switching AR - Influenza Mortality
 library(MSwM)
